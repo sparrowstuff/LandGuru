38e2fabc5c5bec06bf9cf87810be86927957f573
sparrowstuff <damnagett@gmail.com>
2025-04-20 13:25:12 +0300

    header upd
    доделать показание ссылок в хедере (adversite, support)
    кнопка бургера работает как надо

--- main.js
+++ main.js
@@ -2,4 +2,7 @@ const burger = document.querySelector('.burger-btn')
 
 burger.addEventListener('click', function () {
 	burger.classList.toggle('transform')
+
+	const burgerMenu = document.querySelector('.header__burger-menu')
+	burgerMenu.classList.toggle('header__burger-menu--opened')
 })
