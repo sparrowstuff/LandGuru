e94d444676fc35f0c4c76a074a0512b1618307bf
sparrowstuff <damnagett@gmail.com>
2025-04-28 16:09:34 +0300

    swiper commentary block complete

--- main.js
+++ main.js
@@ -6,3 +6,48 @@ burger.addEventListener('click', function () {
 	const burgerMenu = document.querySelector('.header__burger-menu')
 	burgerMenu.classList.toggle('header__burger-menu--opened')
 })
+
+const swiper = new Swiper('.swiper', {
+	// Optional parameters
+	direction: 'horizontal',
+	slidesPerView: 3,
+	spaceBetween: 10,
+	loop: true,
+
+	// If we need pagination
+	pagination: {
+		el: '.swiper-pagination',
+	},
+
+	// Navigation arrows
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+
+	speed: 600,
+
+	breakpoints: {
+		// от брейкпоинта в 2000 пикселей идет 3 слайда
+		2000: {
+			slidesPerView: 3,
+		},
+
+		1000: {
+			slidesPerView: 3,
+		},
+
+		// от брейкпоинта в 768 пикселей идет 2 слайда
+		768: {
+			slidesPerView: 2,
+		},
+
+		480: {
+			slidesPerView: 1,
+		},
+
+		320: {
+			slidesPerView: 1,
+		},
+	},
+})
